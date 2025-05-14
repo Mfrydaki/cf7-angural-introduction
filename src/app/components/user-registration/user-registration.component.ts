3564bb837ed7cf3e1470d7cefedb1b6e9369e616
Maria Frydaki <mfrydaki@gmail.com>
2025-05-14 21:38:32 +0300

    updates

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup,ReactiveFormsModule,Validators} from '@angular/forms';
+
 
 
 
@@ -13,5 +15,18 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './user-registration.component.css'
 })
 export class UserRegistrationComponent {
+
+  form = new FormGroup({
+    username: new FormControl('',Validators.required),
+    name: new FormControl('',Validators.required),
+    surname: new FormControl('',Validators.required),
+    email : new FormControl('',[Validators.required,Validators.email]),
+    address: new FormGroup({
+      area: new FormControl(''),
+      road: new FormControl('')
+    }),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+    confirmPassword: new FormControl ('',[Validators.required,Validators.minLength(6)])
+  })
 
 }
