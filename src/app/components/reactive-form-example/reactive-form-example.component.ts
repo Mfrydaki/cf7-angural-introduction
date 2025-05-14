a0586fbf1cb01bd4519cdd7db2557e8bcfae1c33
Maria Frydaki <mfrydaki@gmail.com>
2025-05-14 19:32:26 +0300

    updates

--- src/app/components/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
 import { PersonTableComponent } from '../person-table/person-table.component';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+import { EPerson } from '../../shared/interfaces/eperson';
 
 
 @Component({
@@ -11,5 +12,17 @@ import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.c
   styleUrl: './reactive-form-example.component.css'
 })
 export class ReactiveFormExampleComponent {
+  // personService = inject(PersonService);
+  currentPerson: EPerson | undefined;
+  persons: EPerson[] = [];
 
+  onPerson(data: EPerson){
+    // console.log("Father", data);
+    this.currentPerson = data;
+    // this.persons.push(data);
+    this.persons = [...this.persons, data]
+    // this.personService.modifiedDataTable.set(true)
+    // console.log("Father", this.persons);
+  }
 }
+
---
+++ src/app/shared/interfaces/jokes.ts
@@ -0,0 +1,9 @@
+export interface DadJoke {
+    joke: string;
+
+}
+
+export interface ChuckNorrisJoke {
+    value : string;
+
+}
